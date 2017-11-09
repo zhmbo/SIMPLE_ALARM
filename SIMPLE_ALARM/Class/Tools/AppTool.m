6abd90bec69f1bb49e6ec7dcc7a345a4edb37cca
zhangbao <it_zhangbao@163.com>
2017-11-09 15:36:10 +0800

    完善关闭闹铃刷新功能

--- SIMPLE_ALARM/Class/Tools/AppTool.m
+++ SIMPLE_ALARM/Class/Tools/AppTool.m
@@ -12,10 +12,38 @@
 
 + (void)reloadHomeData
 {
-    UITableViewController *vc = (UITableViewController *)NSClassFromString(@"Home_VC");
-    if (vc) {
-        [vc.tableView reloadData];
+    if ([NSStringFromClass([[AppTool getCurrentVC] class]) isEqualToString:@"HOME_VC"]) {
+        UITableViewController *tvc = (UITableViewController *)[AppTool getCurrentVC];
+        [tvc.tableView reloadData];
     }
+}
++ (UIViewController *)getCurrentVC {
+    
+    UIWindow *window = [[UIApplication sharedApplication].windows firstObject];
+    if (!window) {
+        return nil;
+    }
+    UIView *tempView;
+    for (UIView *subview in window.subviews) {
+        if ([[subview.classForCoder description] isEqualToString:@"UILayoutContainerView"]) {
+            tempView = subview;
+            break;
+        }
+    }
+    if (!tempView) {
+        tempView = [window.subviews lastObject];
+    }
+    
+    id nextResponder = [tempView nextResponder];
+    while (![nextResponder isKindOfClass:[UIViewController class]] || [nextResponder isKindOfClass:[UINavigationController class]] || [nextResponder isKindOfClass:[UITabBarController class]]) {
+        tempView =  [tempView.subviews firstObject];
+        
+        if (!tempView) {
+            return nil;
+        }
+        nextResponder = [tempView nextResponder];
+    }
+    return  (UIViewController *)nextResponder;
 }
 
 @end
